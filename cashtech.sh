35d1cecdeb4411fd31edda92ac081f55a9a9d497
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-05-24 09:32:36 -0300

    att

--- cashtech.sh
+++ cashtech.sh
@@ -4,41 +4,9 @@ PURPLE='0;35'
 NC='\033[0m'
 VERSAO=17
 
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá Aluno, serei seu assistente para instalação do Java!;"
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Verificando aqui se você possui o Java instalado...;"
-sleep 2
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá Aluno, serei seu assistente para instalação do CashTech!;"
 
-java -version
-if [ $? -eq 0 ]; then
-	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : Você já tem o java instalado!!!"
-else
-	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
-	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Confirme para mim se realmente deseja instalar o Java (S/N)?"
-	read inst
-	if [ \"$inst\" == \"S\" ]; then
-		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar o Java ;D"
-		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Adicionando o repositório!"
-		sleep 2
-		sudo add-apt-repository ppa:webupd8team/java -y
-		clear
-		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Atualizando! Quase lá."
-		sleep 2
-		sudo apt update -y
-		clear
-
-		if [ $VERSAO -eq 17 ]; then
-			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 17 do Java. Confirme a instalação quando solicitado ;D"
-			sudo apt install openjdk-17-jdk
-			-y
-			clear
-			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
-		fi
-	else
-		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Você optou por não instalar o Java por enquanto, até a próxima então!"
-	fi
-fi
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Agora, vamos verificar se o docker esta instalado...;"
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Vamos verificar se o docker está instalado...;"
 sleep 2
 
 docker --version
@@ -60,12 +28,13 @@ else
 fi
 
 echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Criando ambiente para execução do projeto"
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Baixando imagem do DockerHub..."
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Baixando imagem do banco de dados no DockerHub..."
 sudo docker pull pedrorocs/cash_tech
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Criando container da aplicação"
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Criando container do banco de dados da aplicação"
 sudo docker run -d -p 3306:3306 --name ContainerCashtechBd pedrorocs/cash_tech
 echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) A instalação foi concluida com sucesso!"
 clear
+
 echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Digite 1 para instação com interface gráfica e 2 para command line!"
 
 sudo find -name '*cashtech*' -delete
@@ -76,11 +45,48 @@ if [ \"$tipo\" == \"1\" ]; then
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar com interface gráfica ;D"
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Baixando o executavel....."
 	wget https://github.com/CashTec/CashTechExecutavel/raw/main/cashtech-jar-gui.jar
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Verificando aqui se você possui o Java instalado...;"
+	sleep 2
+
+	java -version
+	if [ $? -eq 0 ]; then
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : Você já tem o java instalado!!!"
+	else
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Confirme para mim se realmente deseja instalar o Java (S/N)?"
+		read inst
+		if [ \"$inst\" == \"S\" ]; then
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar o Java ;D"
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Adicionando o repositório!"
+			sleep 2
+			sudo add-apt-repository ppa:webupd8team/java -y
+			clear
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Atualizando! Quase lá."
+			sleep 2
+			sudo apt update -y
+			clear
+
+			if [ $VERSAO -eq 17 ]; then
+				echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 17 do Java. Confirme a instalação quando solicitado ;D"
+				sudo apt install openjdk-17-jdk
+				-y
+				clear
+				echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
+			fi
+		else
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Você optou por não instalar o Java por enquanto, até a próxima então!"
+		fi
+	fi
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Iniciando a execução da aplicação"
+	sleep 2
+	clear
 	java -jar cashtech-jar-gui.jar
 else
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar com command line ;D"
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Baixando o executavel....."
 	sudo docker pull murilosbarbosa/cashtech-cli
-	sudo docker run -it --name ContainerCashTech -p 8080:8080 murilosbarbosa/cashtech-cli 
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Iniciando a execução da aplicação"
+	sleep 2
+	clear
+	sudo docker run -it --name ContainerCashTech -p 8080:8080 murilosbarbosa/cashtech-cli
 fi
